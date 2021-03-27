03542957faa53c2ed1f8b14a43e95b90f15a9b6e
SH-Xu <240495108@qq.com>
2021-03-27 00:08:29 +0000

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ECS781-MiniProject-Group10
+# RESTful API for Personal Book Collection
+
+The project provides RESTful API for book searching and personal reading list.
--- run.py
+++ run.py
@@ -1,1 +1,71 @@
-from flask import Flask
+import json
+
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    index = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    author = db.Column(db.String(80))
+    description = db.Column(db.String(120))
+
+
+db.create_all()
+
+
+@app.route("/")
+def index():
+    return "This is project work for ECS781 group 10"
+
+
+@app.route("/mylibrary")
+def get_books():
+    books = Book.query.all()
+    output = []
+    for book in books:
+        book_data = {
+            "index": book.index,
+            "title": book.title,
+            "author": book.author,
+            "description": book.description
+        }
+        output.append(book_data)
+    return {"Library List": output}
+
+
+@app.route("/mylibrary/<index>", methods=["GET"])
+def get_book(index):
+    book = Book.query.get_or_404(index)
+    return {
+            "index": book.index,
+            "title": book.title,
+            "author": book.author,
+            "description": book.description
+    }
+
+
+@app.route("/mylibrary", methods=["POST"])
+def add_book():
+    book = Book(
+        title=request.json["title"],
+        author=request.json["author"],
+        description=request.json["description"]
+    )
+    db.session.add(book)
+    db.session.commit()
+    return {"index": book.index}
+
+@app.route("/mylibrary/<index>", method=["DELETE"])
+def del_book():
+    book = Book.query.get(index)
+    db.session.delete(book)
+    db.session.commit()
+    return {"message": "item deleted successfully"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
